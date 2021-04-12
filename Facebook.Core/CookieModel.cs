922401d31fb7bf7f71b012944c8b584101bbdee4
SeanWoo <52250045+SeanWoo@users.noreply.github.com>
2021-04-12 16:50:11 +0400

    Cleaning up the project

--- Facebook.Core/CookieModel.cs
+++ Facebook.Core/CookieModel.cs
@@ -2,7 +2,7 @@
 {
     public class CookieModel
     {
-        public string host_key { get; set; }
+        public string domain { get; set; }
         public string name { get; set; }
         public string value { get; set; }
         public string path { get; set; }
